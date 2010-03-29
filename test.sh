33bbfc4f3c2790cecca5ddfb1557ad81e6cdaf36
Aaron Hamid <aaron.hamid@gmail.com>
2010-03-29 00:50:31 -0400

    uncommented the advice test

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-jruby -w -J-cp /usr/share/derby/lib/derby.jar:lib/xercesImpl-2.9.1.jar -Ilib test/dbdom_test.rb
-#jruby -w -J-cp /usr/share/derby/lib/derby.jar advice_test.rb
+jruby -w -J-cp /usr/share/derby/lib/derby.jar -Ilib test/dbdom_test.rb
+jruby -w -Ilib test/advice_test.rb
